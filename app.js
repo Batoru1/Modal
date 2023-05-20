17893632aa49a6cd5444702676eb666301cff21c
batoru <tbakanas2@gmail.com>
2023-05-20 15:01:20 +0300

    finally tyled css correctly and changed some js variable names, all works good now

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 const modalBtn = document.querySelector('.modal-btn');
 const closeBtn = document.querySelector('.close-btn');
-const content2 = document.querySelector('.content2');
+const modalOverlay = document.querySelector('.modal-overlay');
 
 modalBtn.addEventListener('click', function () {
-  content2.classList.add('open-content2');
+  modalOverlay.classList.add('open-modal');
 });
 closeBtn.addEventListener('click', function () {
-  content2.classList.remove('open-content2');
+  modalOverlay.classList.remove('open-modal');
 });
